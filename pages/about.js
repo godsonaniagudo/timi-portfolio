423a496c71f78b30f1e827a225f9812bc1083dad
Godson Aniagudo <92625291+coderlean@users.noreply.github.com>
2022-05-30 03:20:50 +0100

    Finished about page

--- pages/about.js
+++ pages/about.js
@@ -2,6 +2,8 @@ import Styles from "../styles/about/styles.module.css"
 import Head from 'next/head'
 import avatar from "../components/Layout/images/avatar.png"
 import Image from 'next/image'
+import { aboutSocialIcons } from "../icons"
+import { testimonials } from "../testimonials"
 
 const About = () => {
     return (
@@ -10,11 +12,88 @@ const About = () => {
                 <title>Timmy Adubi | About</title>
             </Head>
 
-            <div>
-                <h1>About Me</h1>
+            <header className={Styles.header}>
+                        <h1>About<br />Me</h1>
 
-                <Image src={avatar}  />
+                        <Image src={avatar}  />
+            </header>
+
+            <div className={Styles.aboutSection}>
+                <div className={Styles.aboutLeft}>
+                    
+
+                    <div className={Styles.divider}>
+
+                    </div>
+
+                    <div className={Styles.actionButtonsContainer}>
+                        <button>Let's Have a chat</button>
+
+                        <button>Get My Resume</button>
+                    </div>
+                    
+                    <div className={Styles.socialIconsContainer}>
+                        {
+                            aboutSocialIcons.map((item, index) => {
+                                return <div>
+                                        {
+                                            item.icon
+                                        }
+                                    </div>
+                            })
+                        }
+                    </div>
+                </div>
+
+                <div className={Styles.aboutRight}>
+        	        <h2>Why Timmy?</h2>
+
+                    <p>Look at it this way: that feeling you get when you get a gift and it's exactly what you've always wanted? That's 
+                        the kind of experience I aim to deliver with each work I put out. Imagine that feeling every time you use an app to drive
+                        value to yourself or your users. That's my mission - to bring ease of use and delight to everyone's lives.
+                    </p>
+
+                    <p>
+                        Timmy Adubi is a Designer, Artist &amps; Entrepereneur.
+
+                        First and foremost, a human with an eye for details, he primarily works on graphics designs and creative design strategies.
+                    </p>
+
+                    <p>In the past, he worked in tech as a design director and designer for companies like Asseco Nigeria, the Nigerian Civil Aviation
+                        Authority and NGCareers.com. He's an alumnus of Walden University, Minnesota where he graduated with a distinction.
+                    </p>
+
+                    <p>Outside of work, he enjoys eating a good meal, making good music and training aspiring UXers in Lagos, Nigeria.</p>
+                </div>
             </div>
+
+            <div className={Styles.testimonialsSection}>
+                <h2>
+                    Testimonials
+                </h2>
+
+                <div className={Styles.testimonialsContainer}>
+                    {
+                        testimonials.map((item, index) => <div className={Styles.testimonial}>
+                            <div className={Styles.testimonialHeader}>
+                                <div style={{borderColor : `${item.color}`}} className={Styles.testimonialAvatar}></div>
+
+                                <div className={Styles.testimonialWriter}>
+                                    <h3>{item.name}</h3>
+
+                                    <span>
+                                        <p>{item.position}</p>
+                                        <p>{item.location}</p>
+                                    </span>
+                                </div>
+                            </div>
+
+                            <p className={Styles.testimonialText}>{item.testmonial}</p>
+                        </div>)
+                    }
+                </div>
+            </div>
+            
         </div>
     )
 }
---
+++ testimonials.js
@@ -0,0 +1,50 @@
+export const testimonials = [
+    {
+        name: "Busayo",
+        position: "CEO Formplus",
+        location: "Lagos, Nigeria",
+        imageUrl: "",
+        testmonial: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+        color: "#2484f1"
+    },
+    {
+        name: "Akintunde",
+        position: "CEO OddsVikings",
+        location: "Helsinki, FI",
+        imageUrl: "",
+        testmonial: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+        color: "#0f7e12"
+    },
+    {
+        name: "Damilola",
+        position: "Business Analyst, ASSECO",
+        location: "Lagos, Nigeria",
+        imageUrl: "",
+        testmonial: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+        color: "#90bffc"
+    },
+    {
+        name: "Paul",
+        position: "CEO NGCareers",
+        location: "Lagos, Nigeria",
+        imageUrl: "",
+        testmonial: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+        color: "#ffb83c"
+    },
+    {
+        name: "Christian",
+        position: "CEO Area Express Nigeria",
+        location: "Lagos, Nigeria",
+        imageUrl: "",
+        testmonial: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+        color: "#ff5050"
+    },
+    {
+        name: "Sunday Paul",
+        position: "CEO Pay4Me Inc.",
+        location: "Idaho, US",
+        imageUrl: "",
+        testmonial: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+        color: "#1eed77"
+    }
+]
